56a20b57794e523a765133f24d11b47583d495e2
Joshua Bello <joshuabello@fb.com>
2022-06-14 15:56:52 -0700

    Can post image and text to back4app server

--- app/src/main/java/com/example/instagram/activities/LoginActivity.java
+++ app/src/main/java/com/example/instagram/activities/LoginActivity.java
@@ -8,40 +8,38 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.instagram.R;
 import com.parse.LogInCallback;
-import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private static final String TAG = "LoginActivity";
-    private EditText etUsername;
-    private EditText etPassword;
-    private Button btnLogin;
+    private static EditText etUsername;
+    private static EditText etPassword;
+    private static Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        initialize();
-
         // if the user already logged in
         if (ParseUser.getCurrentUser() != null){
             goMainActivity();
+        } else {
+            initialize();
         }
     }
 
 
-    private void initialize(){
-        etUsername =  findViewById(R.id.tvUsername);
-        etPassword =  findViewById(R.id.tvPassword);
+    private void initialize() {
+        etUsername = findViewById(R.id.tvUsername);
+        etPassword = findViewById(R.id.tvPassword);
         btnLogin = findViewById(R.id.btnLogin);
+
         loginUser();
     }
 
@@ -51,6 +49,7 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String username = etUsername.getText().toString();
                 String password = etPassword.getText().toString();
+
                 ParseUser.logInInBackground(username, password, new LogInCallback() {
                     @Override
                     public void done(ParseUser user, ParseException e) {
@@ -58,16 +57,16 @@ public class LoginActivity extends AppCompatActivity {
                         if (e != null) {
                             // TODO: better error login
                             Log.e(TAG, "Issue with login", e);
+                            Toast.makeText(LoginActivity.this, "Incorrect username or password", Toast.LENGTH_SHORT).show();
                             return;
                         }
                         // navigate to main activity
                         goMainActivity();
-                        Toast.makeText(LoginActivity.this, "Sucess!", Toast.LENGTH_SHORT);
+                        Toast.makeText(LoginActivity.this, "Sucess!", Toast.LENGTH_SHORT).show();
                     }
                 });
             }
         });
-
     }
 
     private void goMainActivity() {
--- app/src/main/java/com/example/instagram/applications/ParseApplication.java
+++ app/src/main/java/com/example/instagram/applications/ParseApplication.java
@@ -2,14 +2,18 @@ package com.example.instagram.applications;
 
 import android.app.Application;
 
+import com.example.instagram.models.Post;
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
-
-    // Initializes Parse SDK as soon as the application is created
     @Override
     public void onCreate() {
         super.onCreate();
+        // Register parse models
+        ParseObject.registerSubclass(Post.class);
+
+        // Initializes Parse SDK as soon as the application is created
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("uJlys8H1DLzMSDnjsUbj4dOBOIYsmdMST06r5JsE")
                 .clientKey("ildFeEEc8JsgL29ZgFqS5OhrsVlhGrKSbjLzA6Rj")
